455790a7512bf987f3ba04bbd5e770c45eedca95
pavel baliulin <pavel.baliulin@gmail.com>
2019-01-12 14:20:20 +0200

    123

---
+++ index.js
@@ -0,0 +1,11 @@
+var a = prompt("Введите а");
+var b = prompt("Введите b");
+console.log(a * b);
+var r = prompt("Введите r");
+console.log((4 / 3) * 3.14 * r * r * r);
+var a = prompt("Введите a");
+var h = prompt("Введите h");
+console.log(a * h);
+var a = prompt("Введите а");
+var h = prompt("Введите h");
+console.log(0.5 * a * h);
